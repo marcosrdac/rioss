0a8bc019842d5d6338325b5fcf808fe4f8bdd75d
Marcos Conceição <marcosrdac@gmail.com>
2020-03-12 17:55:27 -0300

    some code updates

--- models/segmentation/segmentation.py
+++ models/segmentation/segmentation.py
@@ -2,14 +2,16 @@
 
 import numpy as np
 import netCDF4 as nc
-from os import listdir
-from os.path import expanduser
+from os import listdir, makedirs
+from os.path import expanduser, isdir
 
-inputfolder = "~/data/segmentation_input"
-outputfolder = "~/data/segmentation_output"
+inputfolder = "~/data/3_original_and_mask"
+outputfolder = "~/data/4_original_and_segmentation"
 # transformming "~" into "/home/$USER"
 inputfolder = expanduser(inputfolder)
 outputfolder = expanduser(outputfolder)
+for folder in [inputfolder, outputfolder]:
+    makedirs(folder, exist_ok=True)
 
 ncfns = [ fn for fn in listdir(inputfolder) if fn[-3:]=='.nc' ]
 for ncfn in ncfns:
