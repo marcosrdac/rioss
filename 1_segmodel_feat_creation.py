7f699a4e672ef523db4cc8eb6a7db62231be8dcb
marcosrdac <marcosrdac@gmail.com>
2020-05-28 14:19:51 -0300

    Testing aparently finished dataset creation routine on camobi

--- 1_segmodel_feat_creation.py
+++ 1_segmodel_feat_creation.py
@@ -10,13 +10,30 @@ import seaborn as sns
 from time import time, strftime, gmtime
 from os import listdir, makedirs
 from os.path import expanduser, isdir, splitext, join, basename
-from routines.functions import mwsd, mwa, mcwa
+from routines.functions import mwa, mcwa, mwsd
 
 
 # user defined
 INPUT_DATA_DIR = expanduser('~/data/0_original/')
 INPUT_MASKS_DIR = expanduser('~/data/2_masks/')
 OUTPUT_DIR = expanduser('~/data/3_segmodel_creation')
+
+
+FUNCTIONS = {
+    'val': (lambda img: img),
+    'mwa3x3': (lambda img: mwa(img, 1)),
+    'mwsd3x3': (lambda img: mwsd(img, 1)),
+    'mwa15x15': (lambda img: mwa(img, 7)),
+    'mwsd15x15': (lambda img: mwsd(img, 7)),
+    'mcwa16_25': (lambda img: mcwa(img, 25, 16)),
+}
+
+LABEL_NUMBERS = {
+    'oil': 0,
+    'sea': 1,
+    'city': 2,
+}
+
 
 # preconfiguration
 input_data_dir_fps = listdir(INPUT_DATA_DIR)
@@ -30,82 +47,62 @@ input_masks_dir_fps = [join(INPUT_MASKS_DIR, f) for f in input_masks_dir_fps]
 
 
 for ncf in input_data_dir_fps:
+    print(f'Using file: "{ncf}".')
+    print(f'  Mask files found:')
+    # getting mask data
     base = splitext(basename(ncf))[0]
     mask_files = [f for f in input_masks_dir_fps if base in f]
     for mask_fp in mask_files:
-        name = re.findall(r'.*_([^_]*).jpg', mask_fp)[0]
+        mask_name = re.findall(r'.*_([^_]*).jpg', mask_fp)[0]
+        print(f'    - {mask_name} ({mask_fp})')
+        # why is it still three-channel?
         mask = np.array(Image.open(mask_fp))[:, :, 0]
         mask = np.where(mask > 128, True, False)
-        maks_on = np.sum(mask)
-        print(mask_on)
-   #     has_oil, has_sea, has_city = False, False, False
-   #     if name == 'oil':
-   #         has_oil = True
-   #         oil_mask = mask
-   #     elif name == 'sea':
-   #         has_sea = True
-   #         sea_mask = mask
-   #     elif name == 'city':
-   #         has_city = True
-   #         city_mask = mask
-   # if not any([has_oil, has_sea, has_city]):  # if no jpg mask found
-   #     print(f'File "{ncf}" has no linked masks. Please add then to "{INPUT_MASKS_DIR}". Skipping it.')
-   #     continue
-   # else:
-   #     # SENTINEL-1 CONFIGURATION
-   #     print(f'Using file: "{ncf}".')
-   #     ncd = nc.Dataset(ncf)
-   #     img = ncd['Sigma0_VV_db']
-   #     h, w = img.shape
-   #     functions = {
-   #            'mwa3x3':    {'function': lambda img: mwa(img, 1),}
-   #            'mwa15x15':  {'function': lambda img: mwa(img, 7),}
-   #            'mcwa16_25': {'function': lambda img: mcwa(img, 25, 16),}
-   #     }
+        mask_npoints = np.sum(mask)
+        oil_npoints, sea_npoints, city_npoints = 3 * [0]
+        has_oil, has_sea, has_city = 3 * [False]
+        if mask_name == 'oil':
+            has_oil = True
+            oil_mask = mask
+            oil_npoints = mask_npoints
+        elif mask_name == 'sea':
+            has_sea = True
+            sea_mask = mask
+            sea_npoints = mask_npoints
+        elif mask_name == 'city':
+            has_city = True
+            city_mask = mask
+            city_npoints = mask_npoints
+    if not any([has_oil, has_sea, has_city]):  # if no jpg mask found
+        print(
+            f'    None\n'
+            f'Please add its masks to "{INPUT_MASKS_DIR}".\n'
+            f'Skipping it.')
+        continue
 
-   #     x             = []
-   #     y             = []
-   #     value         = []
-   #     avg3x3        = []
-   #     circavg125i16 = []
-   #     std3x3        = []
-   #     label         = [] # 0 bg_sea, 1 fg_spot
-   #     #
-   #     # k=0
-   #     # add = 'test'+'_'
-   #     # for yi in range(    3000, 4000, 3):
-   #     # for xi in range(1000, 2000, 3):
-   #     # for yi in range(    2, img.shape[0]-2, 3):
-   #     # for xi in range(2, img.shape[1]-2, 3):
-   #     # labeled = 0
-   #     # if bg_sea_msk[yi,xi]:
-   #     # labeli  = 0
-   #     # labeled = 1
-   #     # elif fg_spot_msk[yi,xi]:
-   #     # labeli  = 1
-   #     # labeled = 1
-   #     # if labeled == 1:
-   #     # x.append(xi)
-   #     # y.append(yi)
-   #     # value.append(img[yi,xi])
-   #     # avg3x3.append(img_avg3x3[yi,xi])
-   #     # circavg125i16.append(img_circavg125i16[yi,xi])
-   #     # std3x3.append(np.std(img[ yi-1:yi+2,
-   #     # xi-1:xi+2 ]))
-   #     # label.append(labeli)
-   #     # tf      = time()
-   #     # dt      = tf-ti
-   #     # prog    = 100*k/(img.size/3**2)
-   #     # v       = prog/dt
-   #     # left   = (100-prog)/v
-   #     # print(left)
-   #     # print(f'Lasts {strftime("%Hh %Mmin %Ss", gmtime(left))}.\tIteration = {k}.')
-   #     # k+=1
-   #     ##
-   #     ##
-   #     # np.save(add+'x',np.array(x));             del x
-   #     # np.save(add+'y',np.array(y));             del y
-   #     # np.save(add+'value',np.array(value));     del value
-   #     # np.save(add+'avg3x3',np.array(avg3x3));   del avg3x3
-   #     # np.save(add+'std3x3',np.array(std3x3));   del std3x3
-   #     # np.save(add+'label',np.array(label));     del label
+    # opening ooriginal data
+    ncd = nc.Dataset(ncf)
+    img = ncd['Sigma0_VV_db']  # (Sentinel-1 band name after db conversion)
+    h, w = img.shape
+
+    total_points = oil_npoints + sea_npoints + city_npoints
+    labels = np.empty(total_points)
+    labels[:oil_npoints] = LABEL_NUMBERS['oil']
+    labels[oil_npoints:oil_npoints+sea_npoints] = LABEL_NUMBERS['sea']
+    labels[oil_npoints+sea_npoints:total_points] = LABEL_NUMBERS['city']
+    np.save(join(OUTPUT_DIR, f'{base}_{mask_name}.npy'), labels)
+    del(labels)
+
+    for function_name, function in FUNCTIONS.items():
+        resultimg = function(img)
+        if has_oil:
+            oil_points = resultimg[oil_mask]
+        if has_sea:
+            sea_points = resultimg[sea_mask]
+        if has_city:
+            city_points = resultimg[city_mask]
+        all_masked = np.concatenate([oil_points, sea_points, city_points])
+        np.save(join(OUTPUT_DIR, f'{base}_{function_name}.npy'), all_masked)
+        del(all_masked)
+        del(oil_points)
+        del(city_points)
